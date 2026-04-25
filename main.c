aaae6e062eeebbc66eac0717fa370b0b96ee0270
saketsharma14 <iamfakeguy07@gmail.com>
2026-04-26 00:24:14 +0530

    changed main, added the cli interaction, looping

--- main.c
+++ main.c
@@ -1,28 +1,85 @@
-#include "commands.h"
-#include "utils.h"
-#include "sheet.h"
-#include "parser.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-int main() {
-    // create sheet
-    Sheet *sheet = create_sheet(5, 5);
+#include "parser.h"
+#include "sheet.h"
+#include "utils.h"
+#include "commands.h"
+#include "evaluator.h"
 
-    if (!sheet) {
-        printf("Failed to create sheet\n");
+#define INPUT_SIZE 256
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Usage: ./sheet R C\n");
         return 1;
     }
 
-   ParsedInput p;
+    int R = atoi(argv[1]);
+    int C = atoi(argv[2]);
 
-    p = parse_input("d");
-    execute_command(sheet, &p);
+    if (R < 1 || R > 999 || C < 1 || C > 18278) {
+        printf("Invalid sheet size\n");
+        return 1;
+    }
 
-    p = parse_input("s");
-    execute_command(sheet, &p);
+    Sheet *sheet = create_sheet(R, C);
 
-    p = parse_input("scroll_to B2");
-    execute_command(sheet, &p);
+    char input[INPUT_SIZE];
+    char status[64] = "ok";
 
     print_sheet(sheet);
+    printf("[0.0] (%s) > ", status);
+
+    while (fgets(input, sizeof(input), stdin)) {
+        input[strcspn(input, "\n")] = '\0';
+
+        clock_t start = clock();
+
+        ParsedInput p = parse_input(input);
+
+        if (p.type == INPUT_INVALID) {
+            strcpy(status, "unrecognized cmd");
+        }
+
+        else if (p.type == INPUT_COMMAND) {
+            if (p.cmd == CMD_QUIT) break;
+
+            execute_command(sheet, &p);
+            strcpy(status, "ok");
+        }
+
+        else if (p.type == INPUT_FORMULA) {
+            int row, col;
+
+            if (!cell_to_index(p.target, &row, &col)) {
+                strcpy(status, "invalid cell");
+            } else {
+                EvalResult res = evaluate_expression(sheet, p.expression);
+
+                if (res.is_err) {
+                    set_cell_value(sheet, row, col, 0, true);
+                } else {
+                    set_cell_value(sheet, row, col, res.result, false);
+                }
+
+                strcpy(get_cell(sheet, row, col)->formula, p.expression);
+                strcpy(status, "ok");
+            }
+        }
+
+        clock_t end = clock();
+        double time_taken = (double)(end - start) / CLOCKS_PER_SEC;
+
+        if (sheet->output_enabled) {
+            print_sheet(sheet);
+        }
+
+        printf("[%.1f] (%s) > ", time_taken, status);
+    }
+
+    free_sheet(sheet);
+    return 0;
 }
